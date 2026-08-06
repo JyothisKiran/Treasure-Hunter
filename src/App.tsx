140870c045f7b46256a2b28eac67832a373d18c8
JyothisKiran <jyothiskirandc007@gmail.com>
2026-08-06 17:50:07 +0530

    Added ShadCn and 8bitCN packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/8bit/button";
+
 export default function App() {
   return (
-    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+    <>
+    <div className="bg-white text-black min-h-screen flex items-center justify-center">
         Treasure Hunter
     </div>
+    <Button>HEYYYYYYA</Button>
+    </>
   )
 }
